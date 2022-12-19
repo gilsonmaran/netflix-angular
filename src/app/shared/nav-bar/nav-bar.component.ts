49ae502f767f9422abb896688e06597c860e74b9
Gilson Maran <gilson.maran@gmail.com>
2022-12-18 23:20:08 -0300

    first commit

---
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-nav-bar',
+    templateUrl: './nab-bar.component.html',
+    styleUrls: ['./nab-bar.component.css']
+})
+
+export class NavBarComponent implements OnInit{
+    constructor() {}
+    ngOnInit(): void {}
+}
